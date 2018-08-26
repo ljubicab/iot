ff11219b9e9da70f57bed34add70727b43629ed6
root <ema.felding@gmail.com>
2018-08-26 20:15:49 +0200

    update

--- 1.py
+++ 1.py
@@ -4,64 +4,66 @@ import time
 import datetime
 import pymongo
 from pymongo import MongoClient
+from threading import Thread
 
-ser=serial.Serial(port='/dev/ttyACM0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+ser=serial.Serial(port='/dev/ttyACM1', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
 bytesize=serial.EIGHTBITS,timeout=1)
-
+print ser.name
 mongodb_uri="mongodb://Ema:ef123456@ds231941.mlab.com:31941/mewpurr"
 client = MongoClient(mongodb_uri, connectTimeoutMS=30000)
 db=client.get_database("mewpurr")
 times=db.timestamps
-
+alarms=db.alarms
+distance=db.sensor
 def on_connect(clinet,userdata,flags,rc):
-	client.subscribe("mewpurr/food")
-	print "on connect"
+        client.subscribe("mewpurr/food")
+        print "on connect"
 
 def on_message(client,userdata,msg):
-	print(msg.topic+" "+str(msg.payload))
-	if msg.payload =="release the food":
-		print "tu sam"
-		ser.write("0")
-		vrijeme = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%s')
-                a= {
-                       "date":vrijeme,
-                       "confirmed":"true"
-                       }              
-		pushRECORD(a)
+        print(msg.topic+" "+str(msg.payload))
+        if msg.payload =="release the food":
+                ser.write("0")
 
-
-def pushRECORD(record):
-	times.insert_one(record)
-
-arduino = serial.Serial('/dev/ttyACM0',9600)
-  #      print arduino.name
-   #     print "Connected"
-#except:
- #       print "Failed to connect"
-
-
-try:
+def arduinoFunction():
 	while True:
-		if arduino.readline().strip() == 'move':
-			print "if" 
-			vrijeme = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%s')
-			a= {
-				"date":vrijeme,
-				"confirmed":"true"
+		if ser.readline().strip()=="m":
+               		vrijeme = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%s')
+               		record = {
+       	               		"date":vrijeme,
+             	        	"confirmed":"true"
+               	       		}
+			times.insert_one(record)
+			print "aaaa"
+			num = times.count()
+			print num
+		
+def alarmFunction():
+	while True:
+		for document in alarms.find():
+			timeNow=datetime.datetime.today().strftime('%H:%M')
+			print timeNow
+			if alarms.find_one({"time":timeNow}) != None:
+				ser.write("0")
+			time.sleep(60)
+def sensorFunction():
+	while True:
+		if ser.readline().strip()=="e":
+			distance.delete_many({"food":"empty"})
+			record = {
+				"food":"empty"
 				}
-			pushRECORD(a)	
-			print "else"
-			client=mqtt.Client()
-			client.on_connect=on_connect
-			client.on_message=on_message
-			client.connect("broker.hivemq.com",1883,60)
-			client.loop_forever()
+			distance.insert_one(record)
+			time.sleep(10)
 
-	 
-except:
-	print "fail"
-#client=mqtt.Client()
-#client.on_connect=on_connect
-#client.on_message=on_message
-#client.connect("broker.hivemq.com",1883,60)
-#client.loop_forever()
+arduino_thread=Thread(target=arduinoFunction)
+arduino_thread.start()
+alarm_thread=Thread(target=alarmFunction)
+alarm_thread.start()
+sensor_thread=Thread(target=sensorFunction)
+sensor_thread.start()
+client=mqtt.Client()
+client.on_connect=on_connect
+client.on_message=on_message
+client.connect("broker.hivemq.com",1883,60)
+client.loop_forever()	
+
